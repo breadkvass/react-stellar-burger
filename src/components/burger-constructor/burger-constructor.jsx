0de067faf946067199a9b4c552970df521338565
breadkvass <breadkvass@gmail.com>
2023-08-07 15:26:01 +0300

    Создание BurgerConstructor и BurgerIngredients

---
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -0,0 +1,11 @@
+import styles from "./burger-constructor.module.css"
+
+function BurgerConstructor() {
+    return (
+        <div className={styles.content}>
+            
+        </div>
+    );
+}
+  
+export default BurgerConstructor;
---
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -0,0 +1,11 @@
+import styles from "./burger-ingredients.module.css"
+
+function BurgerIngredients() {
+    return (
+        <div className={styles.content}>
+            
+        </div>
+    );
+}
+  
+export default BurgerIngredients;
