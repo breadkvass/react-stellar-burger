6afa126c4bb66d8d562c14e6a4cabbdd1899d662
breadkvass <107198110+breadkvass@users.noreply.github.com>
2023-08-07 15:21:36 +0300

    Revert "AppHeader, BurgerConstructor, BurgerIngredients"

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,15 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
-import AppHeader from "../app-header/app-header";
 
 function App() {
   return (
     <div className={styles.app}>
-      <AppHeader />
-      <main>
-        
-      </main>
+      <pre style={{
+      	margin: "auto",
+      	fontSize: "1.5rem"
+      }}>
+      	Измените src/components/app/app.jsx и сохраните для обновления.
+      </pre>
     </div>
   );
 }
