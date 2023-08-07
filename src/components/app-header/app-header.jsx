fcf20ae94bf64134c220e3b7b6c5a8c7f258c92f
breadkvass <breadkvass@gmail.com>
2023-08-07 15:00:08 +0300

    Appheader: исправлены ссылки на css

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -3,9 +3,10 @@ import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
 import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons";
 import { ListIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons";
 import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons";
+import style from "./app-header.module.css"
 
 const NavLink = (props) => (
-    <div className="pl-5 pr-5 pt-4 pb-4 nav__link">
+    <div className={'pl-5 pr-5 pt-4 pb-4 ' + style.nav__link}>
         {props.icon}
         <p className="text text_type_main-default">{props.text}</p>
     </div>
@@ -13,16 +14,14 @@ const NavLink = (props) => (
 
 function AppHeader() {
     return (
-        <header className="header">
-            <div className="pt-4 pb-4 header__content">
-                <nav className="nav__links nav__links_type_left">
+        <header className={style.header}>
+            <div className={'pt-4 pb-4 ' + style.header__content}> 
+                <nav className={style.nav__links + ' ' + style.nav__links_type_left}>
                     <NavLink text='Конструктор' icon={ <BurgerIcon type="secondary" /> } />
                     <NavLink text='Лента заказов' icon={ <ListIcon type="secondary" /> } />
-                        
-        
                 </nav>
                 <Logo alt="Логотип" style={{ alignSelf: "center" }}/>
-                <nav className="nav__links nav__links_type_right">
+                <nav className={style.nav__links + ' ' + style.nav__links_type_right}>
                     <NavLink text='Личный кабинет' icon={ <ProfileIcon type="secondary" /> } />
                 </nav>
             </div>
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className={styles.app}>
       <AppHeader />
+      <main>
+        
+      </main>
     </div>
   );
 }
