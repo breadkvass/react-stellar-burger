65ba910093758680c3529c382f91d6bbafe28109
breadkvass <breadkvass@gmail.com>
2023-10-04 22:04:07 +0300

    update AddAction

--- src/components/add-action/add-action.jsx
+++ src/components/add-action/add-action.jsx
@@ -1,11 +1,14 @@
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import styles from './add-action.module.css'
+import styles from './add-action.module.css';
+import { NavLink as Link } from 'react-router-dom';
 
-function AddAction({text, button}) {
+function AddAction({text, button, link}) {
     return (
         <div className={styles.action}>
             <p className='text text_type_main-default text_color_inactive'>{text}</p>
-            <Button htmlType="button" type="secondary" size="medium" padding="none" >{button}</Button>
+            <Link to={{ pathname: link }}>
+                <Button htmlType="button" type="secondary" size="medium" padding="none" >{button}</Button>
+            </Link>
         </div>
     )
 }
