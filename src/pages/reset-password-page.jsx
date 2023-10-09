64828c5dbd327709447ef1c632f8713b96eb6999
breadkvass <breadkvass@gmail.com>
2023-10-09 20:52:03 +0300

    upd ResetPasswordPage

--- src/pages/reset-password-page.jsx
+++ src/pages/reset-password-page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
 import Form from '../components/form/form';
 import AddAction from '../components/add-action/add-action';
@@ -6,18 +6,9 @@ import MainLayout from '../components/main-layout/main-layout';
 import Inputs from '../components/inputs/inputs';
 
 function ResetPasswordPage() {
-    const [values, setValues] = React.useState({name: '', password:'' });
-    const getHandler = (name) => {
-        return (event) => {
-          setValues({ ...values, [name]: event.target.value });
-        };
-      };
-    const inputRef = React.useRef(null);
-
-    const [passwordShown, setPasswordShown] = React.useState(false);
-    const togglePassword = () => {
-        setPasswordShown(!passwordShown);
-      };
+    const [ passwordValue, setPasswordValue ] = useState('');
+    const [ codeValue, setCodeValue ] = useState('');
+    const [passwordShown, setPasswordShown] = useState(false);
 
     return (
         <MainLayout>
@@ -34,13 +25,12 @@ function ResetPasswordPage() {
                     <Input
                         type={passwordShown ? 'text' : 'password'}
                         placeholder={'Введите новый пароль'}
-                        onChange={getHandler('password')}
-                        value={values.password}
+                        onChange={(e) => setPasswordValue(e.target.value)}
+                        value={passwordValue}
                         icon={passwordShown ? 'HideIcon' : 'ShowIcon'}
                         name={'Имя'}
                         error={false}
-                        ref={inputRef}
-                        onIconClick={togglePassword}
+                        onIconClick={(e) => setPasswordShown(!passwordShown)}
                         errorText={'Ошибка'}
                         size={'default'}
                         extraClass="ml-1"
@@ -48,11 +38,10 @@ function ResetPasswordPage() {
                     <Input
                         type={'text'}
                         placeholder={'Введите код из письма'}
-                        onChange={getHandler('name')}
-                        value={values.name}
+                        onChange={(e) => setCodeValue(e.target.value)}
+                        value={codeValue}
                         name={'Имя'}
                         error={false}
-                        ref={inputRef}
                         errorText={'Ошибка'}
                         size={'default'}
                         extraClass="ml-1"
