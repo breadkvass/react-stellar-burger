79a1a2188a0731ea676c6a81a247dea05a53d888
breadkvass <breadkvass@gmail.com>
2023-10-31 16:58:31 +0300

    upd Form component

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,9 +1,11 @@
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './form.module.css';
 
-function Form({title, children, button, addActions}) {
+
+
+function Form({title, children, button, handleSubmit, addActions}) {
     return(
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <h4 className={styles.title + ' text text_type_main-medium'}>{title}</h4>
             {children}
             <Button htmlType="submit" type="primary" size="medium">{button}</Button>
