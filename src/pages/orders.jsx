c1961652b884a32235431a5e826203e65e27b584
breadkvass <breadkvass@gmail.com>
2023-10-31 16:57:52 +0300

    create orders component

---
+++ src/pages/orders.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from 'react-router-dom';
+import MainLayout from '../components/main-layout/main-layout';
+
+function Orders() {
+    return (
+        <MainLayout>
+            <div>Лента заказов</div>
+        </MainLayout>
+    )
+}
+
+export default Orders;
