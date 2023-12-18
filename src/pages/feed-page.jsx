5061425f392a741a26f9f499a96b5035087e4aab
breadkvass <breadkvass@gmail.com>
2023-12-18 19:08:22 +0300

    add layout to FeedPage

--- src/pages/feed-page.jsx
+++ src/pages/feed-page.jsx
@@ -1,9 +1,19 @@
 import MainLayout from '../components/main-layout/main-layout';
+import Orders from '../components/orders/orders';
+import styles from './feed-page.module.css';
 
 function FeedPage() {
     return (
         <MainLayout>
-            Лента заказов
+            <div className={styles.feed}>
+                <h2 className='text text_type_main-large'>Лента заказов</h2>
+                <div className={styles.content}>
+                    <Orders />
+                    <div className={styles.stats}>
+
+                    </div>
+                </div>
+            </div>
         </MainLayout>
     ) 
 }
