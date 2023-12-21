93783bbb8eaf007b031310c9966c51f2c9e6cf93
breadkvass <breadkvass@gmail.com>
2023-12-21 23:08:34 +0300

    add OrdersCard with WS

--- src/components/orders-card/orders-card.jsx
+++ src/components/orders-card/orders-card.jsx
@@ -1,23 +1,41 @@
+import { useSelector } from 'react-redux';
+import { v4 as uuid } from 'uuid';
+import { FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './orders-card.module.css';
 
-function OrdersCard({number, date, name, img1, img2, img3, price}) {
+function OrdersCard({order}) {
+    const ingredients = useSelector(state => state.ingredients.ingredients);
+    const getIngredientsById = (ingredients, id) => {
+        return ingredients.find(ingredient => ingredient._id === id);
+    };
+    const orderIngredients = order.ingredients.map(item => getIngredientsById(ingredients, item));
+    const orderPrice = orderIngredients.map(item => item.price).reduce((a, b) => a + b);
 
     return (
         <div className={styles.order}>
             <div className={styles.info}>
-                <p className='text text_type_digits-default'>{number}</p>
-                <p className='text text text_type_main-small text_color_inactive'>{date}</p>
+                <p className='text text_type_digits-default'>{'#' + order.number}</p>
+                <FormattedDate date={new Date(order.createdAt)} className='text text_type_main-default text_color_inactive'/>
             </div>
-            <p className='text text_type_main-medium'>{name}</p>
+            <p className='text text_type_main-medium'>{order.name}</p>
             <div className={styles.info}>
                 <ul className={styles.ingredients}>
-                    <li className={styles.ingredient} key={1} style={{zIndex: 6 - 1, listStyle: "none"}}><img className={styles.img} src={img1} alt='ингредиент'/></li>
-                    <li className={styles.ingredient} key={2} style={{zIndex: 6 - 2, listStyle: "none"}}><img className={styles.img} src={img2} alt='ингредиент'/></li>
-                    <li className={styles.ingredient} key={3} style={{zIndex: 6 - 3, listStyle: "none"}}><img className={styles.img} src={img3} alt='ингредиент'/></li>
+                    {orderIngredients.map((item, index) => {
+                        if (index <= 4) {
+                            return (
+                                <li className={styles.ingredient} key={uuid()} style={{ zIndex: 6 - index, backgroundImage: `url(${item.image})` }}></li>
+                            )
+                        }
+                    })}
+                    {orderIngredients.length > 6 &&
+                        <li className={styles.ingredient} key={uuid()} style={{zIndex: 1, backgroundImage: `url(${orderIngredients[5].image_mobile})`}}>
+                            <p className={styles.counter + ' text text_type_main-default'}>{'+' + (orderIngredients.length - 5)}</p>
+                        </li>
+                    }
                 </ul>
                 <div className={styles.price}>
-                    <p className="text text_type_digits-default">{price}</p>
+                    <p className="text text_type_digits-default">{orderPrice}</p>
                     <CurrencyIcon />
                 </div>
             </div>
--- src/components/orders/orders.jsx
+++ src/components/orders/orders.jsx
@@ -1,25 +1,13 @@
+import { useSelector } from 'react-redux';
 import OrdersCard from '../orders-card/orders-card';
 import styles from './orders.module.css';
 
 function Orders() {
+    const orders = useSelector(state => state.feed.orders);
+
     return (
         <div className={styles.orders + ' custom-scroll'}>
-            <OrdersCard
-                number='#034535'
-                date='Сегодня, 16:20 i-GMT+3'
-                name='Death Star Starship Main бургер'
-                img1='https://code.s3.yandex.net/react/code/core.png'
-                img2='https://code.s3.yandex.net/react/code/sp_1.png'
-                img3='https://code.s3.yandex.net/react/code/mineral_rings.png'
-                price='480' />
-            <OrdersCard
-                number='#034534'
-                date='Сегодня, 13:20 i-GMT+3'
-                name='Interstellar бургер'
-                img1='https://code.s3.yandex.net/react/code/sauce-01.png'
-                img2='https://code.s3.yandex.net/react/code/sauce-03.png'
-                img3='https://code.s3.yandex.net/react/code/bun-01.png'
-                price='560' />
+            {orders.map(item => (<OrdersCard key={item._id} order={item} />))};
         </div>
     )
     
