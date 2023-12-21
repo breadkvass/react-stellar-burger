577af2888e6f39ce2df67a20ae919aea887aca37
breadkvass <breadkvass@gmail.com>
2023-12-22 00:47:55 +0300

    add Stats with WS

--- src/components/stats-board-column/stats-board-column.jsx
+++ src/components/stats-board-column/stats-board-column.jsx
@@ -1,13 +1,11 @@
 import styles from './stats-board-column.module.css';
 
-function StatsBoardColumn({title, number1, number2, number3, color}) {
+function StatsBoardColumn({title, numbers, color}) {
     return (
         <div className={styles.column}>
             <h3 className='text text_type_main-medium'>{title}</h3>
             <ul className={styles.list}>
-                <li className='text text_type_digits-default' style={{color: color}}>{number1}</li>
-                <li className='text text_type_digits-default' style={{color: color}}>{number2}</li>
-                <li className='text text_type_digits-default' style={{color: color}}>{number3}</li>
+                {numbers.map(item => <li key={item} className='text text_type_digits-default' style={{color: color}}>{item}</li>)}
             </ul>
         </div>
     )
--- src/components/stats/stats.jsx
+++ src/components/stats/stats.jsx
@@ -1,13 +1,19 @@
+import { useSelector } from 'react-redux';
 import Completed from '../completed/completed';
 import StatsBoardColumn from '../stats-board-column/stats-board-column';
 import styles from './stats.module.css';
 
 function Stats() {
+    const orders = useSelector(state => state.feed.orders);
+    const ordersReady = orders.filter(order => order.status === 'done').map(item => item.number).splice(0,5);
+    const ordersInProcess = orders.filter(order => order.status !== 'done').map(item => item.number).splice(0,5);
+
     return (
         <div className={styles.stats}>
             <div className={styles.columns}>
-                <StatsBoardColumn title='Готовы:' number1='034533' number2='034532' number3='034530' color='#00CCCC'/>
-                <StatsBoardColumn title='В процессе:' number1='034538' number2='034541' number3='034542' color='white'/>
+                <StatsBoardColumn title='Готовы:' numbers={ordersReady} color='#00CCCC'/>
+                
+                <StatsBoardColumn title='В процессе:' numbers={ordersInProcess} color='white'/>
             </div>
             <Completed text='Выполнено за все время:' number='28 752' />
             <Completed text='Выполнено за сегодня:' number='138' />
