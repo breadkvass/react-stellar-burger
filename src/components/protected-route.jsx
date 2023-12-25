599eb5ba6af0dd474250db92e1d0622eda9a61ef
breadkvass <breadkvass@gmail.com>
2023-12-25 14:16:22 +0300

    upd protected route: change dispatch

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,15 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
-import { updateToken } from "../utils/api";
+import { getUser } from "../utils/api";
 
 export const ProtectedOnlyUnAuth = ({ component }) => {
   const dispatch = useDispatch();
   const isAuth = useSelector(store => store.auth.isAuth);
   const location = useLocation();
   const refreshToken = localStorage.getItem('refreshToken');
+  const accessToken = localStorage.getItem('accessToken');
   
   if (isAuth === false && refreshToken) {
-    dispatch(updateToken(refreshToken));
+    dispatch(getUser(accessToken));
     return <Navigate to="/profile" state={{from: location} } />;
   } else if (isAuth === true) {
     return <Navigate to="/profile" state={{from: location} } />;
@@ -23,10 +24,11 @@ export const ProtectedOnlyAuth = ({ component }) => {
   const isAuth = useSelector(store => store.auth.isAuth);
   const location = useLocation();
   const refreshToken = localStorage.getItem('refreshToken');
+  const accessToken = localStorage.getItem('accessToken');
   if (isAuth === true) {
     return component;
   } else if (isAuth === false && refreshToken) {
-    dispatch(updateToken(refreshToken));
+    dispatch(getUser(accessToken));
     return component;
   } else if (isAuth === false && !refreshToken) {
     return <Navigate to="/login" state={{from: location} } />;
@@ -36,10 +38,10 @@ export const ProtectedOnlyAuth = ({ component }) => {
 export const Protected = ({ component }) => {
   const dispatch = useDispatch();
   const isAuth = useSelector(store => store.auth.isAuth);
-  const location = useLocation();
   const refreshToken = localStorage.getItem('refreshToken');
+  const accessToken = localStorage.getItem('accessToken');
   if (isAuth === false && refreshToken) {
-    dispatch(updateToken(refreshToken));
+    dispatch(getUser(accessToken));
     return component;
   } else {
     return component;
