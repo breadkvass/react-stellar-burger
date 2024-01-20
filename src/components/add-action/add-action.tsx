1e987854ac0bdd8930a031120cbed2ffeca4e855
breadkvass <breadkvass@gmail.com>
2024-01-20 23:49:43 +0300

    upd AddActions.tsx

--- src/components/add-action/add-action.tsx
+++ src/components/add-action/add-action.tsx
@@ -1,23 +1,23 @@
 import { NavLink as Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './add-action.module.css';
 
-function AddAction({text, button, link}) {
+type TAddAction = {
+    text: string;
+    button: string;
+    link: string;
+};
+
+const AddAction = ({text, button, link}: TAddAction) => {
     return (
         <div className={styles.action}>
             <p className='text text_type_main-default text_color_inactive'>{text}</p>
             <Link to={{ pathname: link }}>
-                <Button htmlType="button" type="secondary" size="medium" padding="none" >{button}</Button>
+                <Button htmlType="button" type="secondary" size="medium">{button}</Button>
             </Link>
         </div>
     )
 }
 
-AddAction.propTypes = {
-    text: PropTypes.string,
-    button: PropTypes.string,
-    link: PropTypes.string,
-}
 
 export default AddAction;
