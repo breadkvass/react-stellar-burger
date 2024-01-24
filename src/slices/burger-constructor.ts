15a83fb51ede2dde3c304df3566dd0573505bc9e
breadkvass <breadkvass@gmail.com>
2024-01-24 17:19:25 +0300

    add TS to burgerConstructor slice

--- src/slices/burger-constructor.ts
+++ src/slices/burger-constructor.ts
@@ -1,14 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { v4 as uuid } from 'uuid';
 
-const filling = (ingredientId) => ({
+const filling = (ingredientId: string) => ({
     id: ingredientId,
     key: uuid()
 })
 
+type TBurgerConstructor = {
+    bun: string,
+    filling: Array<object>,
+    draggingIndex: number | object
+}
+
 const { actions, reducer } = createSlice({
     name: 'reducer',
-    initialState: {
+    initialState: <TBurgerConstructor>{
         bun: '643d69a5c3f7b9001cfa093c',
         filling: [
             filling('643d69a5c3f7b9001cfa0944'),
@@ -20,27 +26,27 @@ const { actions, reducer } = createSlice({
         draggingIndex: -1
     },
     reducers: {
-        setBun: (state, action) => {
+        setBun: (state, action: PayloadAction<string>) => {
             state.bun = action.payload;
         },
-        removeIngredient: (state, action) => {
+        removeIngredient: (state, action: PayloadAction<number>) => {
             const newFilling = state.filling;
             newFilling.splice(action.payload, 1);
             state.filling = newFilling;
         },
-        addIngredient: (state, action) => {
+        addIngredient: (state, action: PayloadAction<string>) => {
             const newFilling = state.filling;
             newFilling.push(filling(action.payload));
             state.filling = newFilling;
         },
-        moveIngredient: (state, action) => {
+        moveIngredient: (state, action: PayloadAction<{from: number, to: number}>) => {
             const newFilling = state.filling;
             const moved = newFilling.splice(action.payload.from, 1)[0];
             newFilling.splice(action.payload.to, 0, moved);
             state.filling = newFilling;
             state.draggingIndex = action.payload;
         },
-        setDraggingIndex: (state, action) => {
+        setDraggingIndex: (state, action: PayloadAction<number>) => {
             state.draggingIndex = action.payload;
         },
         resetDraggingIndex: state => {
