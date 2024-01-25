6ac9c283f2a4e93d375fa35dc2d75f6a1eacc864
breadkvass <breadkvass@gmail.com>
2024-01-25 21:05:02 +0300

    add ts to BurgerComponent & BurgerConstructor

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useState, useEffect, SyntheticEvent } from 'react';
+import { useSelector, useDispatch } from '../../hooks/hooks';
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import ConstructorContainer from '../constructor-container/constructor-container';
@@ -9,9 +9,10 @@ import { postOrder } from '../../utils/api';
 import styles from "./burger-constructor.module.css";
 
 function BurgerConstructor() {
-  const [isShowModal, setIsShowModal] = useState(false);
-  const [totalPrice, setTotalPrice] = useState(0);
-  const { bun, filling } = useSelector(state => state.burgerConstructor);
+  const [isShowModal, setIsShowModal] = useState<boolean>(false);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  const bun = useSelector(state => state.burgerConstructor.bun);
+  const filling = useSelector(state => state.burgerConstructor.filling);
   const { ingredients, isLoading } = useSelector(state => state.ingredients);
   const accessToken = localStorage.getItem('accessToken');
   const isAuth = useSelector(state => state.auth.isAuth);
@@ -21,12 +22,12 @@ function BurgerConstructor() {
   useEffect(() => {
     if (!isLoading) {
       const orderItems = [bun, ...(filling.map(f => f.id)), bun];
-      const totalPrice = orderItems.map(id => ingredients.find((item) => item._id === id)).reduce((acc, item) => acc + item.price, 0);
+      const totalPrice = orderItems.map(id => ingredients.find(item => item._id === id)).reduce((acc, item) => item ? acc + item.price : acc, 0);
       setTotalPrice(totalPrice);
     }
   }, [isLoading, ingredients, bun, filling])
 
-  const openModal = (e) => {
+  const openModal = (e: SyntheticEvent<Element, Event>) => {
     e.stopPropagation();
     dispatch(postOrder([bun, ...(filling.map(f => f.id)), bun], accessToken));
     setIsShowModal(true);
@@ -42,7 +43,7 @@ function BurgerConstructor() {
       <div className={styles.info + " pt-10 pb-10"}>
         <div className={styles.price}>
           <p className="text text_type_digits-medium">{totalPrice}</p>
-          <CurrencyIcon />
+          <CurrencyIcon type="primary"/>
         </div>
         <Button onClick={openModal} disabled={!isAuth ? true : false} htmlType="button" type="primary" size="large">Оформить заказ</Button>
       </div>
